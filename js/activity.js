16263276f00c8399b99d4eba729c91a28495b962
harsh singh <22je0388@iitism.ac.in>
2024-04-05 13:50:25 +0530

    activity.setup commented

--- js/activity.js
+++ js/activity.js
@@ -4,14 +4,14 @@ define(["sugar-web/activity/activity", "sugar-web/graphics/colorpalette", "textu
     requirejs(['domReady!'], function(doc) {
 
         // Initialize the activit   y.
-        activity.setup();
+        // activity.setup();
 
-        // var diceColor = document.getElementById("dice-color");
-        // var changeDiceColor = new colorpalette.ColorPalette(diceColor);
-        // changeDiceColor.setColor('rgb(255,255,255)');
-        // changeDiceColor.addEventListener('colorChange', function(e) {
-        //     $t.color = e.detail.color;
-        // });
+        var diceColor = document.getElementById("dice-color");
+        var changeDiceColor = new colorpalette.ColorPalette(diceColor);
+        changeDiceColor.setColor('rgb(255,255,255)');
+        changeDiceColor.addEventListener('colorChange', function(e) {
+            $t.color = e.detail.color;
+        });
 
         var add_desktexture = new texturepalette.TexturePalette(document.getElementById("floor-color"), "Add Textture");
         add_desktexture.addEventListener('addMat', function(event) {
